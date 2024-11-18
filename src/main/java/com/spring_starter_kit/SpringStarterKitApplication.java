78c49def56ade9e22d3fcab039503837a1833628
Elea641 <eleavoliotis64@gmail.com>
2024-11-18 15:02:27 +0100

    first commit

---
+++ src/main/java/com/spring_starter_kit/SpringStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.spring_starter_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_starter_kit/SpringStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_starter_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
